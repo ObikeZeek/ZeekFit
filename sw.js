189b6dded21b1d105362209cb38ad543b981cdc3
ObikeZeek <oadhuze@gmail.com>
2026-08-15 15:06:19 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="zeekfit-v2";
+const CACHE="zeekfit-v2.2";
 const FILES=["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
